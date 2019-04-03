48ef2f25677476cb20a8907f8b12d408689b4cba
Tonny <tonny035@hotmail.com>
2019-04-03 09:45:17 +0200

    Add project files.

---
+++ WF_TicTacToe/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true; // true = X turn; false = Y turn
+        int turnCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Chappi3", "Tic Tac Toe About");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+
+            turn = !turn;
+            b.Enabled = false;
+            turnCount++;
+            checkForWinner();
+        }
+
+        private void checkForWinner()
+        {
+            bool thereIsA_Winner = false;
+
+            // Horizontal checks
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                thereIsA_Winner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                thereIsA_Winner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                thereIsA_Winner = true;
+
+            // Vertical checks
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                thereIsA_Winner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                thereIsA_Winner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                thereIsA_Winner = true;
+
+            // Diagonal checks
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                thereIsA_Winner = true;
+            else if ((C1.Text == B2.Text) && (B2.Text == A3.Text) && (!C1.Enabled))
+                thereIsA_Winner = true;
+
+            if (thereIsA_Winner)
+            {
+
+                string winner = "";
+                if (turn)
+                {
+                    winner = "O";
+                    o_win_count.Text = (Int32.Parse(o_win_count.Text) + 1).ToString();
+                }
+                else
+                {
+                    winner = "X";
+                    x_win_count.Text = (Int32.Parse(x_win_count.Text) + 1).ToString();
+                }
+
+                MessageBox.Show(winner + " Wins!", "Congratulations!");
+                disableButtons();
+                newGameAfterWin();
+            }// end if
+            else
+            {
+                if (turnCount == 9)
+                {
+                    draw_count.Text = (Int32.Parse(draw_count.Text) + 1).ToString();
+                    MessageBox.Show("Draw!", "Draw!");
+                }
+            }
+
+        }// end of checkForWinner
+
+        private void disableButtons()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }// end forech
+            }// end try
+            catch {
+                Console.WriteLine("Could not disable buttons after a win!");
+            }
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turnCount = 0;
+            resetCounters();
+
+            foreach (Control c in Controls)
+                {
+                    try
+                    {
+                        Button b = (Button)c;
+                        b.Enabled = true;
+                        b.Text = "";
+                    }// end try
+                    catch {
+                    Console.WriteLine("Could not reset the buttons when a new game was started!");
+                }
+            }// end forech
+        }
+
+        private void button_enter(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+            }// end if
+        }
+
+        private void button_leave(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+                b.Text = "";
+            }// end if
+        }
+
+        private void resetCounters()
+        {
+            o_win_count.Text = "0";
+            x_win_count.Text = "0";
+            draw_count.Text = "0";
+        }
+
+        private void newGameAfterWin()
+        {
+            
+            turnCount = 0;
+
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }// end try
+                catch
+                {
+                    Console.WriteLine("Could not reset the buttons when a new game was started!");
+                }
+            }// end forech
+        }
+    }
+}
